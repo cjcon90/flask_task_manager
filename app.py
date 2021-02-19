9c790927b0fa76f79192674cfe0d04287a3ff288
cjcon90 <cjcon90@pm.me>
2021-02-19 20:57:32 +0000

    allow user to submit tasks to DB

--- app.py
+++ app.py
@@ -81,6 +81,20 @@ def profile(username):
 
 @app.route('/add_task', methods=['GET', 'POST'])
 def add_task():
+    if request.method == 'POST':
+        is_urgent = 'on' if request.form.get('is_urgent') else 'off'
+        task = {
+            'category_name': request.form.get('category_name'),
+            'task_name': request.form.get('task_name'),
+            'task_description': request.form.get('task_description'),
+            'is_urgent': is_urgent,
+            'due_date': request.form.get('due_date'),
+            'created_by': session['user']
+        }
+        mongo.db.tasks.insert_one(task)
+        flash('Your new task has been added!')
+        return redirect('get_tasks')
+        
     categories = mongo.db.categories.find().sort("category_name", 1) # sort by ascending
     return render_template('add_task.html', categories=categories)
 
